b21909e22fd592367044c377426cb79abab16e79
joshsegatt <josuesegatofilho@gmail.com>
2025-10-15 00:24:40 +0100

    Update Builderlayout.tsx

--- src/components/Builder/Builderlayout.tsx
+++ src/components/Builder/Builderlayout.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from "react";
 import { Download, Copy, Save } from "lucide-react";
-import { Button } from "../../components/ui/Button"; // caminho relativo correto
+import { Button } from "../../ui/Button"; // caminho relativo correto
 
 interface BuilderLayoutProps {
   form: ReactNode;
