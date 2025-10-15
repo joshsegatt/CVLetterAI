223e13aa57f51f966d4bbae0774adb1e55f414ee
joshsegatt <josuesegatofilho@gmail.com>
2025-10-15 20:58:28 +0100

    Replace single quotes with double quotes in Switch.tsx

--- src/components/ui/Switch.tsx
+++ src/components/ui/Switch.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type SwitchProps = {
   checked?: boolean;
@@ -7,7 +7,7 @@ type SwitchProps = {
   className?: string;
 };
 
-export default function Switch({ checked = false, onChange, disabled = false, className = '' }: SwitchProps) {
+export default function Switch({ checked = false, onChange, disabled = false, className = "" }: SwitchProps) {
   return (
     <label className={`inline-flex items-center cursor-pointer ${className}`}>
       <input
@@ -19,13 +19,13 @@ export default function Switch({ checked = false, onChange, disabled = false, cl
       />
       <div
         aria-hidden
-        className={`w-10 h-6 transition-colors rounded-full ${checked ? 'bg-indigo-600' : 'bg-gray-300'} ${
-          disabled ? 'opacity-50' : ''
+        className={`w-10 h-6 transition-colors rounded-full ${checked ? "bg-indigo-600" : "bg-gray-300"} ${
+          disabled ? "opacity-50" : ""
         }`}
       >
         <div
           className={`w-4 h-4 bg-white rounded-full shadow transform transition-transform ${
-            checked ? 'translate-x-4' : 'translate-x-1'
+            checked ? "translate-x-4" : "translate-x-1"
           }`}
         />
       </div>
