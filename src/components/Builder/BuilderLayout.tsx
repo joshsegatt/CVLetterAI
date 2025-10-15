6f101b1d42cc6be3795a5953218a6bc877daee49
joshsegatt <josuesegatofilho@gmail.com>
2025-10-15 22:03:09 +0100

    Refactor BuilderLayout for compatibility re-export

--- src/components/Builder/BuilderLayout.tsx
+++ src/components/Builder/BuilderLayout.tsx
@@ -1,16 +1,4 @@
-import React from "react";
-
-type Props = { children?: React.ReactNode };
-
-export default function BuilderLayout({ children }: Props) {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <header className="p-4 border-b bg-white">
-        <div className="max-w-5xl mx-auto">
-          <h1 className="text-lg font-semibold">Builder</h1>
-        </div>
-      </header>
-      <main className="max-w-5xl mx-auto p-6">{children}</main>
-    </div>
-  );
-}
+// compatibility re-export
+import BuilderLayout from "../builder/BuilderLayout";
+export default BuilderLayout;
+export { BuilderLayout };
