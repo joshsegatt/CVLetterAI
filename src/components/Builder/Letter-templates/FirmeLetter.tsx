f776c23af373abc1d19c95665ce0b112095b98c3
joshsegatt <josuesegatofilho@gmail.com>
2025-10-15 20:56:12 +0100

    Add FirmLetter component for letter preview

---
+++ src/components/Builder/Letter-templates/FirmeLetter.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function FirmLetter() {
+  return (
+    <div className="border rounded p-4 bg-white">
+      <div className="text-lg font-semibold">Firm Letter (preview)</div>
+      <div className="mt-2 text-sm text-gray-500">Firm-tone letter preview.</div>
+    </div>
+  );
+}
